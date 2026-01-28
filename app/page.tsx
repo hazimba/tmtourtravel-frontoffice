cad40b7608050441a83d10dd408edd25b59257ae
Hazim Bakar <hazimbaa@gmail.com>
2026-01-29 00:51:01 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -124,66 +124,6 @@ const HomePage = async () => {
         <ProductImageRender products={packagesExtended} />
         <div className="h-20 bg-gray-100"></div>
       </div>
-    <div className="w-screen">
-      <FormPopup />
-      <div className="">
-        <div className="marquee-container bg-gray-300 py-6 text-black py-2 overflow-hidden whitespace-nowrap">
-          <div className="marquee-track">
-            <span className="marquee-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
-              quasi distinctio ullam temporibus perferendis minima, possimus
-              explicabo ut deserunt maxime repellendus incidunt modi aliquid non
-              suscipit dolorem quod molestias illo.
-            </span>
-            <span className="marquee-text">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem
-              quasi distinctio ullam temporibus perferendis minima, possimus
-              explicabo ut deserunt maxime repellendus incidunt modi aliquid non
-              suscipit dolorem quod molestias illo.
-            </span>
-          </div>
-        </div>
-        <div className="h-108 w-full mx-auto">
-          <Image
-            src="https://www.pixelstalk.net/wp-content/uploads/images6/4K-Travel-Wallpaper-HD-Free-download.jpg"
-            alt="Example"
-            width={4000}
-            height={400}
-            className="object-cover h-full w-full"
-          />
-        </div>
-        <div className="max-w-5xl w-full mx-auto flex items-center overflow-x-auto gap-6 px-4 py-8 scrollbar-hide">
-          {[Facebook, Twitter, Instagram, Linkedin, Youtube, Github].map(
-            (Icon, idx) => (
-              <div
-                key={idx}
-                className="flex-shrink-0 w-56 h-56 rounded-full bg-white flex items-center justify-center shadow hover:bg-gray-100 transition-colors cursor-pointer border border-gray-600"
-              >
-                <Icon size={32} />
-              </div>
-            )
-          )}
-        </div>
-
-        <div className="mb-16 flex flex-col max-w-7xl w-full mx-auto">
-          <div className="font-semibold text-3xl">JELAJAH MANIA</div>
-          {packageLoop()}
-        </div>
-        <div className="mb-16 flex flex-col max-w-7xl w-full mx-auto">
-          <div className="font-semibold text-3xl">GROUND</div>
-          {packageLoop()}
-        </div>
-        <div className="mb-16 flex flex-col max-w-7xl w-full mx-auto">
-          <div className="font-semibold text-3xl">UMRAH</div>
-          {packageLoop()}
-        </div>
-        <div className="mb-16 flex flex-col max-w-7xl w-full mx-auto">
-          <div className="font-semibold text-3xl">MICE</div>
-          {packageLoop()}
-        </div>
-        <ProductImageRender products={packagesExtended} />
-        <div className="h-20 bg-gray-100"></div>
-      </div>
     </div>
   );
 };
