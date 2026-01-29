f6bf8c10ff4ba84b43fb0f30a200cc9aa35e1124
Hazim Bakar <hazimbaa@gmail.com>
2026-01-30 03:29:49 +0800

    fix sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   siteUrl: "https://tmtourtravel-frontoffice.vercel.app",
   generateRobotsTxt: true,
-  // Optionally:
-  // exclude: ['/some-path-to-exclude'],
-  // additionalPaths: async (config) => [
-  //   await config.transform(config, '/dynamic-route-1'),
-  // ],
+  additionalPaths: async (config) => [
+    await config.transform(config, "/"), // homepage
+    // await config.transform(config, "/about"), // example page
+    // await config.transform(config, "/packages"), // your packages page
+    // await config.transform(config, "/contact"), // contact page
+    // add all your main pages here, e.g.:
+    // await config.transform(config, "/umrah"),
+    // await config.transform(config, "/team-building"),
+  ],
 };
