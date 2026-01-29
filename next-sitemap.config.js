9ef65888312681046118c0b1da08979560aca623
Hazim Bakar <hazimbaa@gmail.com>
2026-01-30 03:17:57 +0800

    sitemap install

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://tmtourtravel-frontoffice.vercel.app/", // replace with your real domain
+  generateRobotsTxt: true,
+  // optional: exclude any paths you don't want to include in sitemap
+  // exclude: ['/secret-page'],
+};
